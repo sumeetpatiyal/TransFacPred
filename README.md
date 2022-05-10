8aa480b40769f41f4562cad8023957a3070cc118
sumeetpatiyal <summitpatiyal@gmail.com>
2022-05-10 09:15:33 +0530

    TransFacPred Files

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ece8908e08db091fc96ebee865044c96419d27fc911d8ea84b6e831aa78fb0be
+size 2794
---
+++ transfacpred.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a54e2160afb70fc6a5faf5c93b313e1930ee97e04da66faee9dc770492c3ddb9
+size 7666
